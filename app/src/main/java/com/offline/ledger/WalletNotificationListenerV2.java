e238abfd30a03e553fdad7c5b0ccb9459b296f3b
hjj915566202-lab <hjj915566202@gmail.com>
2026-07-26 21:35:07 +0900

    Add fresh notification listener component

---
+++ app/src/main/java/com/offline/ledger/WalletNotificationListenerV2.java
@@ -0,0 +1,9 @@
+package com.offline.ledger;
+
+/**
+ * New listener component for v1.2.0. A new component name makes Android create
+ * fresh notification-listener filter state instead of retaining v1.1.0's
+ * alerting-only defaults.
+ */
+public class WalletNotificationListenerV2 extends WalletNotificationListener {
+}
